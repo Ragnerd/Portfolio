e9ea892dbb1cb311c7c158567896caf3215a098c
Ragnerd <foysalakondo97@gmail.com>
2025-08-04 23:24:28 +0600

    reupload

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -25,14 +25,16 @@ export default function Home() {
             </p>
             {/* button and Socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <Link href="/Curriculum Vitae.pdf">Download CV</Link>
-                <FiDownload className="text-xl" />
-              </Button>
+              <Link href="/Curriculum Vitae.pdf">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </Link>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
